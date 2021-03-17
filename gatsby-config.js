e7ed35a83223b21a168b319e430ca5f5bd0a3cfa
digthedill <popssly@gmail.com>
2021-03-17 13:40:10 -0500

    killed cms and optimized seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,32 +21,7 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL:
-          `https://karissa-talanian-cms.herokuapp.com` ||
-          `http://localhost:1337`,
-        queryLimit: 1000, // Default to 100
-        // contentTypes: [`macrames`, `arts`, `graphics`],
-        singleTypes: [`splash-img`, `about-me`],
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [],
-      },
-    },
+
     {
       resolve: `gatsby-source-cloudinary`,
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,28 @@
-module.exports.onCreateNode = async ({ node, actions, createNodeId }) => {
-  const crypto = require(`crypto`)
+// module.exports.onCreateNode = async ({ node, actions, createNodeId }) => {
+//   const crypto = require(`crypto`)
 
-  if (node.internal.type === "StrapiAboutMe") {
-    const newNode = {
-      id: createNodeId(`about-${node.id}`),
-      parent: node.id,
-      slug: node.slug,
-      title: node.title,
-      published_at: node.published_at,
-      children: [],
-      internal: {
-        content: node.bio || " ",
-        type: "about",
-        mediaType: "text/markdown",
-        contentDigest: crypto
-          .createHash("md5")
-          .update(node.bio || " ")
-          .digest("hex"),
-      },
-    }
-    actions.createNode(newNode)
-    actions.createParentChildLink({
-      parent: node,
-      child: newNode,
-    })
-  }
-}
+//   if (node.internal.type === "StrapiAboutMe") {
+//     const newNode = {
+//       id: createNodeId(`about-${node.id}`),
+//       parent: node.id,
+//       slug: node.slug,
+//       title: node.title,
+//       published_at: node.published_at,
+//       children: [],
+//       internal: {
+//         content: node.bio || " ",
+//         type: "about",
+//         mediaType: "text/markdown",
+//         contentDigest: crypto
+//           .createHash("md5")
+//           .update(node.bio || " ")
+//           .digest("hex"),
+//       },
+//     }
+//     actions.createNode(newNode)
+//     actions.createParentChildLink({
+//       parent: node,
+//       child: newNode,
+//     })
+//   }
+// }
